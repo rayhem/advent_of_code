2c0f1dd526a84989090dc70d7ca9f0651f4a2b02
rayhem <49202382+rayhem@users.noreply.github.com>
2020-12-03 22:21:25 -0500

    Solve part 2

--- src/solutions/day03.rs
+++ src/solutions/day03.rs
@@ -4,19 +4,27 @@ pub struct Day03 {}
 
 impl Solution for Day03 {
     fn part_one(&self, input: &str) -> Option<String> {
-        let width = input.lines().next().unwrap().chars().count();
+        Some(count_trees(input, (3usize, 1usize)).to_string())
+    }
 
+    fn part_two(&self, input: &str) -> Option<String> {
+        let slopes: Vec<(usize, usize)> = vec![(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
         Some(
-            input
-                .lines()
-                .enumerate()
-                .filter(|(i, line)| line.chars().nth((i * 3) % width) == Some('#'))
-                .count()
+            slopes
+                .into_iter()
+                .map(|slope| count_trees(input, slope))
+                .product::<usize>()
                 .to_string(),
         )
     }
+}
 
-    fn part_two(&self, _input: &str) -> Option<String> {
-        None
-    }
+fn count_trees(input: &str, (dx, dy): (usize, usize)) -> usize {
+    let width = input.lines().next().unwrap().chars().count();
+    input
+        .lines()
+        .step_by(dy)
+        .enumerate()
+        .filter(|(i, line)| line.chars().nth((i * dx) % width) == Some('#'))
+        .count()
 }
