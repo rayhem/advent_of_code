c20fe58b47c3028891aeb4031e1fe8d76d3ca303
rayhem <49202382+rayhem@users.noreply.github.com>
2020-12-06 12:05:13 -0500

    Complete day06 part 1

--- src/solutions/day06.rs
+++ src/solutions/day06.rs
@@ -1,10 +1,24 @@
+use std::collections::HashSet;
+
 use crate::solutions::Solution;
 
 pub struct Day06 {}
 
 impl Solution for Day06 {
-    fn part_one(&self, _input: &str) -> Option<String> {
-        None
+    fn part_one(&self, input: &str) -> Option<String> {
+        Some(
+            input
+                .split("\n\n")
+                .map(|group| {
+                    group
+                        .lines()
+                        .map(|line| line.chars().collect::<HashSet<char>>())
+                        .fold(HashSet::new(), |acc, x| acc.union(&x).map(|c| *c).collect())
+                        .len()
+                })
+                .sum::<usize>()
+                .to_string(),
+        )
     }
 
     fn part_two(&self, _input: &str) -> Option<String> {
