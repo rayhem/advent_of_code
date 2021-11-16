9239f832db36a9c8d18728c8ca6925100f4a242d
rayhem <49202382+rayhem@users.noreply.github.com>
2021-11-15 23:30:55 -0500

    Complete day 1

--- src/solutions/day01.rs
+++ src/solutions/day01.rs
@@ -4,10 +4,31 @@ pub struct Day01 {}
 
 impl Solution for Day01 {
     fn part_one(&self, input: &str) -> Option<String> {
-        None
+        Some(
+            input
+                .chars()
+                .fold(0, |total, ch| total + as_number(ch))
+                .to_string(),
+        )
     }
 
     fn part_two(&self, input: &str) -> Option<String> {
+        let mut total = 0;
+        for (i, ch) in input.chars().enumerate() {
+            total += as_number(ch);
+            if total == -1 {
+                return Some((i + 1).to_string());
+            }
+        }
+
         None
+    }
+}
+
+fn as_number(ch: char) -> i32 {
+    match ch {
+        '(' => 1,
+        ')' => -1,
+        _ => 0,
     }
 }
