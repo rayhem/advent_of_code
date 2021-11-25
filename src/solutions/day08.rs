b5eadc0fdcf9be71e244acfba83f4382e695ac5a
rayhem <49202382+rayhem@users.noreply.github.com>
2021-11-25 13:16:37 -0500

    Add template solutions

---
+++ src/solutions/day08.rs
@@ -0,0 +1,30 @@
+use crate::solutions::Solution;
+
+pub struct Day08 {}
+
+impl Solution for Day08 {
+    fn part_one(&self, input: &str) -> Option<String> {
+        todo!()
+    }
+
+    fn part_two(&self, input: &str) -> Option<String> {
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    mod part1 {
+        use super::super::*;
+
+        #[test]
+        fn example0() {}
+    }
+
+    mod part2 {
+        use super::super::*;
+
+        #[test]
+        fn example0() {}
+    }
+}
---
+++ src/solutions/day09.rs
@@ -0,0 +1,30 @@
+use crate::solutions::Solution;
+
+pub struct Day09 {}
+
+impl Solution for Day09 {
+    fn part_one(&self, input: &str) -> Option<String> {
+        todo!()
+    }
+
+    fn part_two(&self, input: &str) -> Option<String> {
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    mod part1 {
+        use super::super::*;
+
+        #[test]
+        fn example0() {}
+    }
+
+    mod part2 {
+        use super::super::*;
+
+        #[test]
+        fn example0() {}
+    }
+}
