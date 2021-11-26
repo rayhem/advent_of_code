d2378e88f38ed27184cf89c7eaac2c8c6aeefc69
rayhem <49202382+rayhem@users.noreply.github.com>
2021-11-26 17:59:25 -0500

    Generalize execute_with_timing

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,17 @@
 pub mod cli;
 pub mod solution;
 
-pub fn execute_with_timing(
+pub fn execute_with_timing<P: std::fmt::Debug + std::convert::AsRef<std::path::Path>>(
     day: i32,
-    input_file: &std::path::Path,
+    input_file: &P,
     solution: &dyn solution::Solution,
 ) {
     let input = std::fs::read_to_string(&input_file)
-        .unwrap_or_else(|_| panic!("File {} not found", input_file.display()));
+        .unwrap_or_else(|_| panic!("File {:?} not found", input_file));
 
     let now = std::time::Instant::now();
     println!(
-        "Day {0} ({2:>7}): {1:?}",
+        "Day {0:02} ({2:>7}): {1:?}",
         day,
         solution.run(input.as_str()),
         now.elapsed().as_micros()
