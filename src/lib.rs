52491794c4ef7115d183eae55758087909b1a6f7
rayhem <49202382+rayhem@users.noreply.github.com>
2021-11-26 18:11:05 -0500

    Move execute_with_timing to solution module

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,2 @@
 pub mod cli;
 pub mod solution;
-
-pub mod advent_utils {
-    pub fn execute_with_timing<P: std::fmt::Debug + std::convert::AsRef<std::path::Path>>(
-        day: i32,
-        input_file: &P,
-        solution: &dyn crate::solution::Solution,
-    ) {
-        let input = std::fs::read_to_string(&input_file)
-            .unwrap_or_else(|_| panic!("File {:?} not found", input_file));
-
-        let now = std::time::Instant::now();
-        println!(
-            "Day {0:02} ({2:>7}): {1:?}",
-            day,
-            solution.run(input.as_str()),
-            now.elapsed().as_micros()
-        );
-    }
-}
--- src/solution.rs
+++ src/solution.rs
@@ -6,3 +6,20 @@ pub trait Solution {
         [self.part_one(input), self.part_two(input)]
     }
 }
+
+pub fn execute_with_timing<P: std::fmt::Debug + std::convert::AsRef<std::path::Path>>(
+    day: i32,
+    input_file: &P,
+    solution: &dyn crate::solution::Solution,
+) {
+    let input = std::fs::read_to_string(&input_file)
+        .unwrap_or_else(|_| panic!("File {:?} not found", input_file));
+
+    let now = std::time::Instant::now();
+    println!(
+        "Day {0:02} ({2:>7}): {1:?}",
+        day,
+        solution.run(input.as_str()),
+        now.elapsed().as_micros()
+    );
+}
