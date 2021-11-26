ee134e56aabcb9c58acdcc4f59ab0d5bfc4233ed
rayhem <49202382+rayhem@users.noreply.github.com>
2021-11-26 18:00:43 -0500

    Add execute to top level module

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,21 @@
 pub mod cli;
 pub mod solution;
 
-pub fn execute_with_timing<P: std::fmt::Debug + std::convert::AsRef<std::path::Path>>(
-    day: i32,
-    input_file: &P,
-    solution: &dyn solution::Solution,
-) {
-    let input = std::fs::read_to_string(&input_file)
-        .unwrap_or_else(|_| panic!("File {:?} not found", input_file));
+mod advent_utils {
+    pub fn execute_with_timing<P: std::fmt::Debug + std::convert::AsRef<std::path::Path>>(
+        day: i32,
+        input_file: &P,
+        solution: &dyn crate::solution::Solution,
+    ) {
+        let input = std::fs::read_to_string(&input_file)
+            .unwrap_or_else(|_| panic!("File {:?} not found", input_file));
 
-    let now = std::time::Instant::now();
-    println!(
-        "Day {0:02} ({2:>7}): {1:?}",
-        day,
-        solution.run(input.as_str()),
-        now.elapsed().as_micros()
-    );
+        let now = std::time::Instant::now();
+        println!(
+            "Day {0:02} ({2:>7}): {1:?}",
+            day,
+            solution.run(input.as_str()),
+            now.elapsed().as_micros()
+        );
+    }
 }
