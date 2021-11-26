ce35317e48940dbfa752f9d4a43181f19286a048
rayhem <49202382+rayhem@users.noreply.github.com>
2021-11-26 13:13:16 -0500

    Add solution module

--- src/solution.rs
+++ src/solution.rs
@@ -1,8 +1,10 @@
-pub trait Solution {
-    fn part_one(&self, input: &str) -> Option<String>;
-    fn part_two(&self, input: &str) -> Option<String>;
+mod solution {
+    pub trait Solution {
+        fn part_one(&self, input: &str) -> Option<String>;
+        fn part_two(&self, input: &str) -> Option<String>;
 
-    fn run(&self, input: &str) -> [Option<String>; 2] {
-        [self.part_one(input), self.part_two(input)]
+        fn run(&self, input: &str) -> [Option<String>; 2] {
+            [self.part_one(input), self.part_two(input)]
+        }
     }
 }
