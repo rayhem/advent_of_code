3e9895df6a83ac1eb809db3c7fe3e8ef26a8e40e
rayhem <49202382+rayhem@users.noreply.github.com>
2021-11-26 13:06:41 -0500

    Add solution trait

---
+++ src/solution.rs
@@ -0,0 +1,8 @@
+pub trait Solution {
+    fn part_one(&self, input: &str) -> Option<String>;
+    fn part_two(&self, input: &str) -> Option<String>;
+
+    fn run(&self, input: &str) -> [Option<String>; 2] {
+        [self.part_one(input), self.part_two(input)]
+    }
+}
