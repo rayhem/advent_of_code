538d4116871744ae7ea668728313659a31005892
rayhem <49202382+rayhem@users.noreply.github.com>
2021-12-01 09:14:23 -0500

    Better implementation and test coverage for day 01

--- src/solutions/day01.rs
+++ src/solutions/day01.rs
@@ -1,32 +1,32 @@
 use advent_utils::solution::Solution;
-use itertools::{Itertools, TupleWindows};
 
 pub struct Day01 {}
 
 impl Solution for Day01 {
     fn part_one(&self, input: &str) -> Option<String> {
-        Some(get_positive_deltas(input.lines().map(|l| l.parse::<i32>().unwrap())).to_string())
+        parse_and_evaluate(input, 1)
     }
 
     fn part_two(&self, input: &str) -> Option<String> {
-        Some(
-            get_positive_deltas(
-                input
-                    .lines()
-                    .map(|l| l.parse::<i32>().unwrap())
-                    .tuple_windows::<(_, _, _)>()
-                    .map(|(a, b, c)| a + b + c),
-            )
-            .to_string(),
-        )
+        parse_and_evaluate(input, 3)
     }
 }
 
-fn get_positive_deltas<T>(xs: T) -> i32
+fn parse_and_evaluate(input: &str, skip_size: usize) -> Option<String> {
+    Some(
+        get_positive_deltas(input.lines().map(|l| l.parse::<i32>().unwrap()), skip_size)
+            .to_string(),
+    )
+}
+
+fn get_positive_deltas<T>(xs: T, skip_size: usize) -> i32
 where
-    T: Iterator<Item = i32>,
+    T: Iterator<Item = i32> + Clone,
 {
-    xs.tuple_windows().filter(|(a, b)| b > a).count() as i32
+    xs.clone()
+        .zip(xs.skip(skip_size))
+        .filter(|(a, b)| b > a)
+        .count() as i32
 }
 
 #[cfg(test)]
@@ -35,7 +35,12 @@ mod test {
     static DATA: [i32; 10] = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
 
     #[test]
-    fn positive_deltas() {
-        assert_eq!(get_positive_deltas(DATA.into_iter()), 7)
+    fn adjacent_increase() {
+        assert_eq!(get_positive_deltas(DATA.into_iter(), 1), 7)
+    }
+
+    #[test]
+    fn three_element_increase() {
+        assert_eq!(get_positive_deltas(DATA.into_iter(), 3), 5)
     }
 }
