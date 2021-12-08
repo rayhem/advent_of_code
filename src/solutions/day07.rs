a3b37db0051441089096165a471c89e2e3247a94
rayhem <49202382+rayhem@users.noreply.github.com>
2021-12-08 08:22:21 -0500

    Add tests for day 7

--- src/solutions/day07.rs
+++ src/solutions/day07.rs
@@ -7,18 +7,17 @@ impl Solution for Day07 {
     fn part_one(&self, input: &str) -> Option<String> {
         let crab_positions = crab_positions(input.trim());
         let median = *crab_positions.get(crab_positions.len() / 2).unwrap();
-        Some(linear_fuel_cost(crab_positions.into_iter(), median).to_string())
+        Some(linear_fuel_cost(crab_positions.iter(), median).to_string())
     }
 
     fn part_two(&self, input: &str) -> Option<String> {
-        let crab_positions = crab_positions(input.trim()).into_iter();
-
+        let crab_positions = crab_positions(input.trim());
         let mean_bounds = (0..=1)
-            .map(|i| (crab_positions.clone().sum::<i32>() + i) / (crab_positions.len() as i32));
+            .map(|i| (crab_positions.iter().sum::<i32>() + i) / (crab_positions.len() as i32));
 
         Some(
             mean_bounds
-                .map(|m| quadratic_fuel_cost(crab_positions.clone(), m))
+                .map(|m| quadratic_fuel_cost(crab_positions.iter(), m))
                 .min()
                 .unwrap()
                 .to_string(),
@@ -33,18 +32,18 @@ fn crab_positions(s: &str) -> Vec<i32> {
         .collect()
 }
 
-fn linear_fuel_cost<T>(crab_positions: T, target_position: i32) -> i32
+fn linear_fuel_cost<'a, T>(crab_positions: T, target_position: i32) -> i32
 where
-    T: Iterator<Item = i32>,
+    T: Iterator<Item = &'a i32>,
 {
     crab_positions
         .map(|position| (position - target_position).abs())
         .sum()
 }
 
-fn quadratic_fuel_cost<T>(crab_positions: T, target_position: i32) -> i32
+fn quadratic_fuel_cost<'a, T>(crab_positions: T, target_position: i32) -> i32
 where
-    T: Iterator<Item = i32>,
+    T: Iterator<Item = &'a i32>,
 {
     crab_positions
         .map(|position| {
@@ -52,4 +51,45 @@ where
             n * (n + 1) / 2
         })
         .sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod unit {
+        use super::*;
+        static DATA: &str = "16,1,2,0,4,2,7,1,2,14";
+
+        #[test]
+        fn example1() {
+            let crab_positions = crab_positions(DATA);
+            assert_eq!(linear_fuel_cost(crab_positions.iter(), 1), 41);
+            assert_eq!(linear_fuel_cost(crab_positions.iter(), 2), 37); // minimal cost
+            assert_eq!(linear_fuel_cost(crab_positions.iter(), 3), 39);
+            assert_eq!(linear_fuel_cost(crab_positions.iter(), 10), 71);
+        }
+
+        #[test]
+        fn example2() {
+            let crab_positions = crab_positions(DATA);
+            assert_eq!(quadratic_fuel_cost(crab_positions.iter(), 5), 168);
+        }
+    }
+
+    mod integration {
+        use super::*;
+        const SOLUTION: Day07 = Day07 {};
+        static INPUT: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/inputs/day07.dat"));
+
+        #[test]
+        fn part_one() {
+            assert_eq!(SOLUTION.part_one(INPUT), Some(String::from("333755")));
+        }
+
+        #[test]
+        fn part_two() {
+            assert_eq!(SOLUTION.part_two(INPUT), Some(String::from("94017638")));
+        }
+    }
 }
