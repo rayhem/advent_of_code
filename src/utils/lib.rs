693248346f2e109126b43ae9a5d31a71b046159f
rayhem <49202382+rayhem@users.noreply.github.com>
2023-10-18 21:41:47 -0400

    Testing verification feature + macro

--- src/utils/lib.rs
+++ src/utils/lib.rs
@@ -1,2 +1,40 @@
 pub mod cli;
 pub mod solution;
+
+#[macro_export]
+macro_rules! my_input {
+    ($year:expr, $day:expr) => {
+        concat!(
+            env!("CARGO_MANIFEST_DIR"),
+            "/inputs/",
+            $year,
+            "/day",
+            $day,
+            ".txt"
+        )
+    };
+}
+
+#[macro_export]
+macro_rules! verify {
+    ($x:ident, $input_file:expr, $sol1:expr, $sol2:expr) => {
+        #[cfg(test)]
+        #[cfg(feature = "verification")]
+        mod verification {
+            use super::*;
+
+            const SOLUTION: $x = $x {};
+            static INPUT: &str = include_str!($input_file);
+
+            #[test]
+            fn part_01() {
+                assert_eq!(SOLUTION.part_one(INPUT), Some($sol1.to_owned()));
+            }
+
+            #[test]
+            fn part_02() {
+                assert_eq!(SOLUTION.part_two(INPUT), Some($sol2.to_owned()));
+            }
+        }
+    };
+}
