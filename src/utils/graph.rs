4602d8b13a68ceb6a80803ee7dc0a34c856a2bc9
rayhem <49202382+rayhem@users.noreply.github.com>
2023-11-07 08:30:23 -0500

    Solve 2015 day 13 parts 1 & 2

--- src/utils/graph.rs
+++ src/utils/graph.rs
@@ -1,6 +1,9 @@
 use core::fmt::Debug;
 use std::collections::HashMap;
 use std::hash::Hash;
+use std::iter;
+
+use itertools::{FoldWhile, Itertools};
 
 /// Details an edge in a graph between nodes `a` and `b`. The `data` field
 /// defaults to the zero-sized unit type, `()`, which is suitable for undirected
@@ -8,28 +11,32 @@ use std::hash::Hash;
 /// weighted and directed graphs or named edges.
 #[derive(Clone, Debug, PartialEq)]
 pub struct Edge<T, U = ()> {
-    pub a: T,
-    pub b: T,
+    pub start: T,
+    pub end: T,
     pub data: U,
 }
 
 impl<T> Edge<T> {
     /// Construct an [`Edge`], leaving the `data` field empty
-    pub fn new(a: T, b: T) -> Self {
-        Self { a, b, data: () }
+    pub fn new(start: T, end: T) -> Self {
+        Self {
+            start,
+            end,
+            data: (),
+        }
     }
 }
 
 impl<T, U> Edge<T, U> {
     /// Construct an [`Edge`] with data
-    pub fn with_data(a: T, b: T, data: U) -> Self {
-        Self { a, b, data }
+    pub fn with_data(start: T, end: T, data: U) -> Self {
+        Self { start, end, data }
     }
 }
 
 #[derive(Clone, Debug)]
-pub struct Graph<T> {
-    pub edges: Vec<Edge<T>>,
+pub struct Graph<T, U> {
+    pub edges: Vec<Edge<T, U>>,
 
     /// Association of a node name with the [`Edge`]s connected to that node.
     /// Note: [`Edge`]s may be undirected, incoming, outgoing, or symmetric
@@ -37,7 +44,7 @@ pub struct Graph<T> {
     pub nodes: HashMap<T, Vec<usize>>,
 }
 
-impl<T: Clone + Debug + Eq + Hash> Graph<T> {
+impl<T: Clone + Debug + Eq + Hash, U: Clone> Graph<T, U> {
     pub fn new() -> Self {
         Graph {
             edges: Vec::new(),
@@ -46,18 +53,20 @@ impl<T: Clone + Debug + Eq + Hash> Graph<T> {
     }
 
     /// Add an edge to the graph
-    pub fn add_edge(&mut self, edge: Edge<T>) {
+    pub fn add_edge(&mut self, edge: Edge<T, U>) {
         let edge_index = self.edges.len();
         self.edges.push(edge.clone());
 
-        self.nodes.entry(edge.a).or_default().push(edge_index);
-        self.nodes.entry(edge.b).or_default().push(edge_index);
+        self.nodes.entry(edge.start).or_default().push(edge_index);
+        self.nodes.entry(edge.end).or_default().push(edge_index);
     }
 }
 
-impl<T: Clone + Debug + Eq + Hash, U: IntoIterator<Item = Edge<T>>> From<U> for Graph<T> {
+impl<T: Clone + Debug + Eq + Hash, U: Clone, V: IntoIterator<Item = Edge<T, U>>> From<V>
+    for Graph<T, U>
+{
     /// Construct a `[Graph]` from an iterator over edges
-    fn from(value: U) -> Self {
+    fn from(value: V) -> Self {
         let mut graph = Graph::new();
 
         for item in value.into_iter() {
@@ -68,9 +77,85 @@ impl<T: Clone + Debug + Eq + Hash, U: IntoIterator<Item = Edge<T>>> From<U> for 
     }
 }
 
-impl<T: Clone + Debug + Eq + Hash> Default for Graph<T> {
+impl<T: Clone + Debug + Eq + Hash, U: Clone> Default for Graph<T, U> {
     fn default() -> Self {
         Self::new()
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct EdgeList<T, U> {
+    nodes: Vec<T>,
+    edges: HashMap<(usize, usize), U>,
+}
+
+impl<T, U, V> From<V> for EdgeList<T, U>
+where
+    T: Clone + Eq + Hash,
+    U: Clone,
+    V: Clone + IntoIterator<Item = Edge<T, U>>,
+{
+    fn from(value: V) -> Self {
+        let nodes = value
+            .clone()
+            .into_iter()
+            .flat_map(|edge| [edge.start, edge.end].into_iter())
+            .unique()
+            .collect::<Vec<_>>();
+
+        let edges = value
+            .into_iter()
+            .map(|edge| {
+                (
+                    (
+                        nodes.iter().position(|x| *x == edge.start).unwrap(),
+                        nodes.iter().position(|x| *x == edge.end).unwrap(),
+                    ),
+                    edge.data,
+                )
+            })
+            .collect();
+
+        Self { nodes, edges }
+    }
+}
+
+impl<T, U: Copy + Ord + Default + std::ops::Add<Output = U>> EdgeList<T, U> {
+    /// Finds the shortest cyclic tour through the graph, evaluating `dist` for
+    /// every edge. The tour distance is then the accumulation of `dist` over
+    /// all edges. This accommodates the general case for directed graphs where
+    /// the forward distance does not equal the reverse distance.
+    pub fn shortest_tour_by(&self, dist: fn(U, U) -> U) -> (Vec<usize>, U) {
+        let tours = (0..self.nodes.len())
+            .chain(iter::once(0))
+            .permutations(self.nodes.len() + 1)
+            .filter(|tour| tour.first().unwrap() == tour.last().unwrap());
+
+        tours
+            .flat_map(|tour| {
+                tour.iter()
+                    .tuple_windows()
+                    .fold_while(Some(U::default()), |acc, (&start, &end)| match acc {
+                        None => FoldWhile::Done(None),
+                        Some(n) => {
+                            FoldWhile::Continue(self.edges.get(&(start, end)).and_then(|forward| {
+                                self.edges
+                                    .get(&(end, start))
+                                    .map(|backward| n + dist(*forward, *backward))
+                            }))
+                        }
+                    })
+                    .into_inner()
+                    .map(|tour_length| (tour, tour_length))
+            })
+            .min_by_key(|(_, tour_length)| *tour_length)
+            .unwrap()
+    }
+
+    /// Finds the shortest tour by accumulating the forward distance for each
+    /// pair of nodes
+    pub fn shortest_tour(&self) -> (Vec<usize>, U) {
+        self.shortest_tour_by(|p, _| p)
     }
 }
 
