a06ad5950918ed1c825efb27260e02921c4ef217
rayhem <49202382+rayhem@users.noreply.github.com>
2023-11-10 13:03:08 -0500

    Improve type safety of CLI actions

--- src/lib/utils/cli.rs
+++ src/lib/utils/cli.rs
@@ -1,6 +1,94 @@
 use clap::{App, Arg, ArgMatches};
 use std::{num::ParseIntError, str::FromStr};
 
+const YEARS: &str = "years";
+const DAYS: &str = "days";
+const INPUT: &str = "input";
+
+#[derive(Clone, Debug, Default)]
+pub struct CommandLineInterface {
+    iface: ArgMatches<'static>,
+}
+
+impl CommandLineInterface {
+    pub fn new() -> Self {
+        Self {
+            iface: App::new("Advent of Code")
+                .version("v0.1")
+                .arg(
+                    Arg::with_name(YEARS)
+                        .default_value("2015-2022")
+                        .help("Specify yearly solutions to execute")
+                        .long("year")
+                        .short("y")
+                        .takes_value(true)
+                        .use_delimiter(true)
+                        .validator(validate_int_sequence)
+                        .value_name("[2015-2022]"),
+                )
+                .arg(
+                    Arg::with_name(DAYS)
+                        .default_value("1-25")
+                        .help("Specify daily solutions to execute")
+                        .long("day")
+                        .short("d")
+                        .takes_value(true)
+                        .use_delimiter(true)
+                        .validator(validate_int_sequence)
+                        .value_name("[1-25]"),
+                )
+                .arg(
+                    Arg::with_name(INPUT)
+                        .default_value("./input")
+                        .help(
+                            "Path to input files directory (assumes subdirectories named by year)",
+                        )
+                        .long(INPUT)
+                        .short("i")
+                        .takes_value(true)
+                        .validator(|dir| {
+                            if std::path::Path::is_dir(std::path::Path::new(&dir)) {
+                                Ok(())
+                            } else {
+                                Err("Input directory does not exist".to_string())
+                            }
+                        })
+                        .value_name("PATH"),
+                )
+                .get_matches(),
+        }
+    }
+
+    pub fn get_years(&self) -> Vec<i32> {
+        self.get_cli_int_sequence(YEARS)
+    }
+
+    pub fn get_days(&self) -> Vec<i32> {
+        self.get_cli_int_sequence(DAYS)
+    }
+
+    pub fn get_input_dir(&self) -> std::path::PathBuf {
+        self.iface.value_of(INPUT).unwrap().into()
+    }
+
+    fn get_cli_int_sequence(&self, flag: &str) -> Vec<i32> {
+        match self.iface.value_of(flag) {
+            Some(v) => {
+                v.split(',')
+                    .flat_map(IntSpecifier::from_str)
+                    .fold(Vec::new(), |mut acc, d| {
+                        match d {
+                            IntSpecifier::Single(n) => acc.push(n),
+                            IntSpecifier::Range(l, u) => acc.extend(l..=u),
+                        };
+                        acc
+                    })
+            }
+            None => Vec::new(),
+        }
+    }
+}
+
 #[derive(Clone, Copy, Debug)]
 enum IntSpecifier {
     Single(i32),
@@ -28,64 +116,4 @@ fn validate_int_sequence(s: String) -> Result<(), String> {
     IntSpecifier::from_str(&s)
         .map(|_| ())
         .map_err(|err| err.to_string())
-}
-
-pub fn make_cli() -> ArgMatches<'static> {
-    App::new("Advent of Code 2015")
-        .version("0.1")
-        .arg(
-            Arg::with_name("years")
-                .default_value("2015-2022")
-                .help("Specify yearly solutions to execute")
-                .long("year")
-                .short("y")
-                .takes_value(true)
-                .use_delimiter(true)
-                .validator(validate_int_sequence)
-                .value_name("[2015-2022]"),
-        )
-        .arg(
-            Arg::with_name("days")
-                .default_value("1-25")
-                .help("Specify daily solutions to execute")
-                .long("days")
-                .short("d")
-                .takes_value(true)
-                .use_delimiter(true)
-                .validator(validate_int_sequence)
-                .value_name("[1-25]"),
-        )
-        .arg(
-            Arg::with_name("input")
-                .default_value("./input")
-                .help("Path to input files directory (assumes subdirectories named by year)")
-                .long("input")
-                .short("i")
-                .takes_value(true)
-                .validator(|dir| {
-                    if std::path::Path::is_dir(std::path::Path::new(&dir)) {
-                        Ok(())
-                    } else {
-                        Err("Input directory does not exist".to_string())
-                    }
-                })
-                .value_name("PATH"),
-        )
-        .get_matches()
-}
-
-pub fn get_cli_int_sequence(flag: &str, matches: &ArgMatches) -> Vec<i32> {
-    if let Some(v) = matches.value_of(flag) {
-        v.split(',')
-            .flat_map(IntSpecifier::from_str)
-            .fold(Vec::new(), |mut acc, d| {
-                match d {
-                    IntSpecifier::Single(n) => acc.push(n),
-                    IntSpecifier::Range(l, u) => acc.extend(l..=u),
-                };
-                acc
-            })
-    } else {
-        Vec::new()
-    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
 use advent::*;
 
 fn main() {
-    let cli = utils::cli::make_cli();
+    let cli = utils::cli::CommandLineInterface::new();
 
     let all_solutions = solutions::all_solutions();
 
-    let root_dir = cli.value_of("input").unwrap();
+    let root_dir = cli.get_input_dir();
 
-    for year in utils::cli::get_cli_int_sequence("years", &cli).into_iter() {
+    for year in cli.get_years() {
         if let Some(year_association) = all_solutions.get(&year) {
-            for day in utils::cli::get_cli_int_sequence("days", &cli).into_iter() {
+            for day in cli.get_days() {
                 if let Some(solution) = year_association.get(&day) {
-                    let fname = format!("{}/{}/day{:02}.txt", root_dir, year, day);
+                    let fname = format!("{}/{}/day{:02}.txt", root_dir.display(), year, day);
                     if let Ok(input) = std::fs::read_to_string(&fname) {
                         solution.execute_with_timing(day, &input);
                     } else {
