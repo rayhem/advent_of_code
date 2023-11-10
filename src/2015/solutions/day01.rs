6094cd8f1fea6992cdeb7cdc0dad599f0c093e89
rayhem <49202382+rayhem@users.noreply.github.com>
2023-11-10 05:48:07 -0500

    Use stateless solution for 2015 day 01

--- src/2015/solutions/day01.rs
+++ src/2015/solutions/day01.rs
@@ -4,24 +4,22 @@ pub struct Day01 {}
 
 impl Solution for Day01 {
     fn part_one(&self, input: &str) -> Option<String> {
-        Some(
-            input
-                .chars()
-                .fold(0, |total, ch| total + as_number(ch))
-                .to_string(),
-        )
+        Some(input.chars().map(as_number).sum::<i32>().to_string())
     }
 
     fn part_two(&self, input: &str) -> Option<String> {
-        let mut total = 0;
-        for (i, ch) in input.chars().enumerate() {
-            total += as_number(ch);
-            if total == -1 {
-                return Some((i + 1).to_string());
-            }
-        }
-
-        None
+        Some(
+            (input
+                .chars()
+                .scan(0, |acc, c| {
+                    *acc = *acc + as_number(c);
+                    Some(*acc)
+                })
+                .take_while(|height| height > &-1)
+                .count()
+                + 1)
+            .to_string(),
+        )
     }
 }
 
