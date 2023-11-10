1e774738a1243e590a4d31234bc92ae496ed72ad
rayhem <49202382+rayhem@users.noreply.github.com>
2023-11-10 05:48:07 -0500

    Solve 2015 day 12 parts 1 & 2

---
+++ src/2015/solutions/day12.rs
@@ -0,0 +1,43 @@
+use utils::solution::Solution;
+pub struct Day12 {}
+
+impl Solution for Day12 {
+    fn part_one(&self, input: &str) -> Option<String> {
+        Some(total_json(input, |_| true).to_string())
+    }
+
+    fn part_two(&self, input: &str) -> Option<String> {
+        Some(
+            total_json(input, |value| {
+                *value != serde_json::Value::String("red".to_string())
+            })
+            .to_string(),
+        )
+    }
+}
+
+fn total_json(input: &str, is_valid: fn(&serde_json::Value) -> bool) -> i64 {
+    let json: serde_json::Value = serde_json::from_str(input).unwrap();
+
+    let mut stack = json.as_object().unwrap().values().collect::<Vec<_>>();
+    let mut total = 0;
+
+    while let Some(item) = stack.pop() {
+        match item {
+            serde_json::Value::Null => continue,
+            serde_json::Value::Bool(_) => continue,
+            serde_json::Value::Number(n) => total += n.as_i64().unwrap(),
+            serde_json::Value::String(_) => continue,
+            serde_json::Value::Array(a) => stack.extend(a),
+            serde_json::Value::Object(obj) => {
+                if obj.values().all(is_valid) {
+                    stack.extend(obj.values());
+                }
+            }
+        }
+    }
+
+    total
+}
+
+utils::verify!(Day12, utils::my_input!("2015", "12"), "119433", "68466");
